ce87694421b8b235953ecb8d71e3458246396d4e
silvaOrdep <pedro.o.silva975@gmail.com>
2025-11-13 13:38:41 -0300

    big bang

---
+++ src/main/java/com/ordep/syncotable/SyncoTableApplication.java
@@ -0,0 +1,13 @@
+package com.ordep.syncotable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncoTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncoTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ordep/syncotable/SyncoTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordep.syncotable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncoTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
