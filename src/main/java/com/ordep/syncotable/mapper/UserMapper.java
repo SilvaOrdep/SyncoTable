1ed6cb12a1b0cdcac3ce3808ae35f877bf019e83
silvaOrdep <pedro.o.silva975@gmail.com>
2025-11-14 12:41:51 -0300

    feat: user mapper

---
+++ src/main/java/com/ordep/syncotable/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.ordep.syncotable.mapper;
+
+import com.ordep.syncotable.dto.UserDto;
+import com.ordep.syncotable.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface UserMapper {
+
+    UserDto mapToDto(User user);
+    @Mapping(target = "id", ignore = true)
+    User mapToEntity(UserDto dto);
+
+}
