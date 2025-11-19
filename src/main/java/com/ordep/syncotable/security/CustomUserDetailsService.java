12d7fea1b0bcf2e95d95af684ee8b85f82e7c0c3
silvaOrdep <pedro.o.silva975@gmail.com>
2025-11-19 17:17:25 -0300

    feat: CustomUserDetailsService para retornar um UserDetails para o spring security

---
+++ src/main/java/com/ordep/syncotable/security/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.ordep.syncotable.security;
+
+import com.ordep.syncotable.model.User;
+import com.ordep.syncotable.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // in the app the username field in the login form contains the email
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+
+        Set<GrantedAuthority> authorities = user.getRoles() == null ? Set.of() :
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName()))
+                        .collect(Collectors.toSet());
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .disabled(!user.isActivated())
+                .build();
+    }
+
+}
