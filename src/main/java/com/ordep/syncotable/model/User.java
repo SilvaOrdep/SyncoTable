c9c1faaa1898c75bae2ed112b09b54c0411aaf54
silvaOrdep <pedro.o.silva975@gmail.com>
2025-11-19 17:14:36 -0300

    fix: inicialização do atributo activated como true para o builder do lombok

--- src/main/java/com/ordep/syncotable/model/User.java
+++ src/main/java/com/ordep/syncotable/model/User.java
@@ -30,6 +30,7 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
     )
     private Set<Role> roles;
+    @lombok.Builder.Default
     private boolean activated = true;
 
 }
