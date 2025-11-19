731e9e2d1fb87b6a20ffd3b36873bf51c0d54934
silvaOrdep <pedro.o.silva975@gmail.com>
2025-11-19 17:12:53 -0300

    refactor: mudança do tipo record para class no DTO de user

--- src/main/java/com/ordep/syncotable/dto/UserDto.java
+++ src/main/java/com/ordep/syncotable/dto/UserDto.java
@@ -2,8 +2,24 @@ package com.ordep.syncotable.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record UserDto(Long id, @NotEmpty(message = "nome de usuário não pode ser vazio") String username,
-                      @NotEmpty(message = "Email não pode ser vazio") @Email(message = "utilize um email válido") String email,
-                      @NotEmpty(message = "campo de senha não pode ser vazio") String password) {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    Long id;
+    @NotEmpty(message = "nome de usuário não pode ser vazio")
+    String username;
+    @NotEmpty(message = "Email não pode ser vazio")
+    @Email(message = "utilize um email válido")
+    String email;
+    @NotEmpty(message = "campo de senha não pode ser vazio")
+    String password;
+
 }
