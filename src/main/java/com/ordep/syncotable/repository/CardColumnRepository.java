f40b80f2d1c6a4477b7207ce2956c4a2d23f15b0
silvaOrdep <pedro.o.silva975@gmail.com>
2025-12-09 11:13:04 -0300

    feat: métodos de acesso a entity via Card para Column e Row

--- src/main/java/com/ordep/syncotable/repository/CardColumnRepository.java
+++ src/main/java/com/ordep/syncotable/repository/CardColumnRepository.java
@@ -1,7 +1,13 @@
 package com.ordep.syncotable.repository;
 
+import com.ordep.syncotable.model.Card;
 import com.ordep.syncotable.model.CardColumn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CardColumnRepository extends JpaRepository<CardColumn, Long> {
+    
+    List<CardColumn> findByCard(Card card);
+    
 }
--- src/main/java/com/ordep/syncotable/repository/CardRowRepository.java
+++ src/main/java/com/ordep/syncotable/repository/CardRowRepository.java
@@ -1,7 +1,13 @@
 package com.ordep.syncotable.repository;
 
+import com.ordep.syncotable.model.Card;
 import com.ordep.syncotable.model.CardRow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CardRowRepository extends JpaRepository<CardRow, Long> {
+
+    List<CardRow> findByCard(Card card);
+
 }
