c16e52264d81588f8a97f0b3b19272793dac9e21
JuanWillian <juanwcaraujo28@gmail.com>
2025-12-10 17:02:00 -0300

    refactor: utilizando strategy para identificar qual handler vai ser usado

--- src/main/java/com/ordep/syncotable/sheets/SpreadsheetFactory.java
+++ src/main/java/com/ordep/syncotable/sheets/SpreadsheetFactory.java
@@ -1,4 +1,31 @@
 package com.ordep.syncotable.sheets;
 
+import com.ordep.syncotable.sheets.impl.CsvHandler;
+import com.ordep.syncotable.sheets.impl.XlsHandler;
+import com.ordep.syncotable.sheets.impl.XlsxHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.util.Map;
+
+@Service
 public class SpreadsheetFactory {
+
+    private final Map<String, Spreadsheet> handlers = Map.of(
+            "csv", new CsvHandler(),
+            "xls", new XlsHandler(),
+            "xlsx", new XlsxHandler()
+    );
+
+    public Spreadsheet getHandler(String filename){
+        String ext = extractExtension(filename);
+        return handlers.get(ext);
+    }
+
+    private String extractExtension(String filename){
+        int i = filename.lastIndexOf(".");
+        return filename.substring(i + 1);
+    }
 }
+
