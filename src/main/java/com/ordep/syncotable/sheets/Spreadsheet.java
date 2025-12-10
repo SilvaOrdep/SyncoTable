6c2bd7dcae33678f25a52e468dc146c59f466026
JuanWillian <juanwcaraujo28@gmail.com>
2025-12-10 16:59:57 -0300

    feat: criando interface para as planilhas

--- src/main/java/com/ordep/syncotable/sheets/Spreadsheet.java
+++ src/main/java/com/ordep/syncotable/sheets/Spreadsheet.java
@@ -1,4 +1,15 @@
 package com.ordep.syncotable.sheets;
 
-public class Spreadsheet {
+import com.ordep.syncotable.model.CardRow;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+
+public interface Spreadsheet {
+    List<CardRow> read(InputStream file) throws IOException;
 }
