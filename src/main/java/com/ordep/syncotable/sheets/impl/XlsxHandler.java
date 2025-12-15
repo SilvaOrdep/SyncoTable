a90a27abb7c215edaea1010d45a6a4cb52e1636d
JuanWillian <juanwcaraujo28@gmail.com>
2025-12-15 14:25:28 -0300

    fix: map estava sendo reutilizado para todos os cardRows

--- src/main/java/com/ordep/syncotable/sheets/impl/XlsxHandler.java
+++ src/main/java/com/ordep/syncotable/sheets/impl/XlsxHandler.java
@@ -3,10 +3,7 @@ package com.ordep.syncotable.sheets.impl;
 import com.ordep.syncotable.model.CardRow;
 import com.ordep.syncotable.sheets.Spreadsheet;
 import org.apache.commons.collections4.IteratorUtils;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
@@ -22,38 +19,35 @@ public class XlsxHandler implements Spreadsheet {
 
         try (Workbook workbook = new XSSFWorkbook(file)) {
             Sheet sheet = workbook.getSheetAt(0);
-
             Iterator<Row> iterator = sheet.iterator();
             Row header = iterator.next();
-            if (iterator.hasNext()) iterator.next();
-
-            int rowIndex = 1;
-            Map<String, Object> rowValues = new HashMap<>();
-
 
             while (iterator.hasNext()) {
                 Row row = iterator.next();
-                List<Object> values = new ArrayList<>();
                 CardRow cardRow = new CardRow();
+                Map<String, Object> rowValues = new LinkedHashMap<>();
 
                 for (int col = 0; col < row.getLastCellNum(); col++) {
                     Cell cell = row.getCell(col, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
 
                     switch (cell.getCellType()) {
                         case STRING:
-                            rowValues.put(header.getCell(col).getStringCellValue() , cell.getStringCellValue());
+                            rowValues.put(header.getCell(col).getStringCellValue(), cell.getStringCellValue());
                             break;
                         case NUMERIC:
-                            rowValues.put(header.getCell(col).getStringCellValue() ,cell.getNumericCellValue());
+                            rowValues.put(header.getCell(col).getStringCellValue(), cell.getNumericCellValue());
                             break;
                         case BOOLEAN:
-                            rowValues.put(header.getCell(col).getStringCellValue() ,cell.getBooleanCellValue());
+                            rowValues.put(header.getCell(col).getStringCellValue(), cell.getBooleanCellValue());
                             break;
                         case FORMULA:
-                            rowValues.put(header.getCell(col).getStringCellValue() ,cell.getCellFormula());
+                            if (cell.getCellType().equals(CellType.NUMERIC)) {
+                                rowValues.put(header.getCell(col).getStringCellValue(), cell.getStringCellValue());
+                            }
+                            rowValues.put(header.getCell(col).getStringCellValue(), cell.getNumericCellValue());
                             break;
                         default:
-                            rowValues.put(null ,null);
+                            rowValues.put("", "");
                     }
                 }
 
@@ -61,7 +55,6 @@ public class XlsxHandler implements Spreadsheet {
                 lines.add(cardRow);
 
                 System.out.println(cardRow.getValuesJson());
-                rowIndex++;
             }
         }
 
