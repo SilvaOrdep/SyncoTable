87cbd7fc737f4968cce78d55f4cabfecbfc2de40
JuanWillian <juanwcaraujo28@gmail.com>
2025-12-18 11:40:23 -0300

    fix: ajustando a logica de importação de csv

--- src/main/java/com/ordep/syncotable/sheets/impl/CsvHandler.java
+++ src/main/java/com/ordep/syncotable/sheets/impl/CsvHandler.java
@@ -2,46 +2,47 @@ package com.ordep.syncotable.sheets.impl;
 
 import com.ordep.syncotable.model.CardRow;
 import com.ordep.syncotable.sheets.Spreadsheet;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CsvHandler implements Spreadsheet {
 
     @Override
-    public List<CardRow> read(  InputStream inputStream) {
+    public List<CardRow> read(InputStream inputStream) {
         List<CardRow> rows = new ArrayList<>();
 
-        try (
-                InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-                BufferedReader br = new BufferedReader(reader)
-        ) {
-            String line;
-            boolean isHeader = true;
-            while ((line = br.readLine()) != null) {
+        try (Reader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.builder()
+                     .setHeader()
+                     .setSkipHeaderRecord(true)
+                     .setIgnoreHeaderCase(true)
+                     .setTrim(true)
+                     .build())) {
 
-                if (isHeader) {
-                    isHeader = false;
-                    continue;
+            List<String> headers = csvParser.getHeaderNames();
+
+            for (CSVRecord csvRecord : csvParser) {
+                Map<String, Object> values = new LinkedHashMap<>();
+
+                for (String header : headers) {
+                    values.put(header, csvRecord.get(header));
                 }
 
-                if (line.isBlank()) {
-                    continue;
-                }
-                String[] columns = line.split(",");
-                CardRow row = new CardRow();
-                Map<String, Object> values = new HashMap<>();
-                for (int i = 0; i < columns.length; i++) {
-                    values.put("Linha " + i, columns[i].trim());
-                }
-                row.setValuesJson(values);
-                rows.add(row);
-                rows.stream().map(CardRow::getValuesJson).forEach(System.out::println);
+                CardRow cardRow = new CardRow();
+                cardRow.setValuesJson(values);
+                rows.add(cardRow);
             }
+
         } catch (IOException e) {
-            throw new RuntimeException("error: " + e.getMessage(), e);
+            e.printStackTrace();
         }
 
         return rows;
