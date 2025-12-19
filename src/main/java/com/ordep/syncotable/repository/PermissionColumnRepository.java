12d76fffeb7c0316105ed170a84295a7179ca52a
silvaOrdep <pedro.o.silva975@gmail.com>
2025-12-19 12:02:09 -0300

    feat: métodos de acesso a PermissionColumn no repository

--- src/main/java/com/ordep/syncotable/repository/PermissionColumnRepository.java
+++ src/main/java/com/ordep/syncotable/repository/PermissionColumnRepository.java
@@ -1,7 +1,17 @@
 package com.ordep.syncotable.repository;
 
+import com.ordep.syncotable.model.CardColumn;
+import com.ordep.syncotable.model.Permission;
 import com.ordep.syncotable.model.PermissionColumn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PermissionColumnRepository extends JpaRepository<PermissionColumn, Long> {
+    List<PermissionColumn> findByPermission(Permission permission);
+
+    Optional<PermissionColumn> findByCardColumnAndPermission(CardColumn cardColumn, Permission permission);
+
+    void deleteByPermission(Permission permission);
 }
