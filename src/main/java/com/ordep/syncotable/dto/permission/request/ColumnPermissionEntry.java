3a89ae0999c0c0a745ff0c25db914f42956074ee
silvaOrdep <pedro.o.silva975@gmail.com>
2025-12-19 12:00:00 -0300

    fix: atributo de permissionId estava ausente nos DTOs de ColumnPermission

--- src/main/java/com/ordep/syncotable/dto/permission/request/ColumnPermissionEntry.java
+++ src/main/java/com/ordep/syncotable/dto/permission/request/ColumnPermissionEntry.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotNull;
 
 public record ColumnPermissionEntry(
     @NotNull(message = "columnId não pode ser nulo") Long columnId,
+    @NotNull(message = "permissionId não pode ser nulo") Long permissionId,
     Boolean canView,
     Boolean canEdit) {
 }
--- src/main/java/com/ordep/syncotable/dto/permission/response/ColumnPermissionEntryResponse.java
+++ src/main/java/com/ordep/syncotable/dto/permission/response/ColumnPermissionEntryResponse.java
@@ -2,6 +2,7 @@ package com.ordep.syncotable.dto.permission.response;
 
 public record ColumnPermissionEntryResponse(
         Long columnId,
+        Long permissionId,
         boolean canView,
         boolean canEdit
 ) {}
